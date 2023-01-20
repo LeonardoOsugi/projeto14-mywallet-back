a30d73c04ad1c72b7ca054627e49a1cc656074e6
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-01-20 02:32:02 -0300

    feat: Database paste

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try{
+    await mongoClient.connect();
+}catch(err){
+    console.log(err);
+};
+
+export const db = mongoClient.db();
+export const collectionUsuarios = db.collection("usuarios");
+export const  collectionMoney = db.collection("money");
+
